8fc35ab122acfdbc70e5631def73c6eae0944182
peng <pc175@uow.edu.au>
2014-06-27 19:10:32 -0400

    fix a bug that cause leftJoin to do only join revise GoogleImage examples to save list of colleges and image search page to S3

--- example/src/main/scala/org/tribbloid/spookystuff/example/largescale/GoogleImage.scala
+++ example/src/main/scala/org/tribbloid/spookystuff/example/largescale/GoogleImage.scala
@@ -13,16 +13,25 @@ object GoogleImage extends SparkSubmittable {
 
   override def doMain() {
 
-    //    val nameRDD = sc.textFile("/home/peng/Documents/affiliation.txt").distinct(16)
-    ((sc.parallelize(Seq("dummy")) +>
+    val names = (sc.parallelize(Seq("dummy")) +>
       Visit("http://www.utexas.edu/world/univ/alpha/") !)
-      .flatMap(page => page.text("div.box2 a", limit = Int.MaxValue, distinct = true)) +>
-      //      .flatMap(_.text("div.box2 a", limit = 10, distinct = true)) +>
+      .flatMap(page => page.text("div.box2 a", limit = Int.MaxValue, distinct = true))
+    //      .flatMap(_.text("div.box2 a", limit = 100, distinct = true))
+
+    names.persist()
+    names.saveAsTextFile("s3n://college-logo/list")
+
+    val searchPages = names +>
       Visit("http://images.google.com/") +>
       DelayFor("form[action=\"/search\"]",50) +>
       TextInput("input[name=\"q\"]","#{_} Logo") +>
       Submit("input[name=\"btnG\"]") +>
-      DelayFor("div#search img",50) !)
+      DelayFor("div#search",50) !
+
+    searchPages.persist()
+    searchPages.save("#{_}.html", "s3n://college-logo-search-page")
+
+    searchPages
       .wgetJoin("div#search img",1,"src")
       .save("#{_}", "s3n://college-logo")
       .foreach(println(_))
