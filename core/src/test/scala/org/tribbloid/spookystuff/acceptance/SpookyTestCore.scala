51f5c7410d7c62553b51d178a36cea9366b06190
peng <pc175@uow.edu.au>
2014-09-06 15:37:11 -0400

    remove SPARK_HOME dependency

--- core/src/test/scala/org/tribbloid/spookystuff/acceptance/SpookyTestCore.scala
+++ core/src/test/scala/org/tribbloid/spookystuff/acceptance/SpookyTestCore.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.{SchemaRDD, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.FunSuite
 import org.tribbloid.spookystuff.SpookyContext
-import org.tribbloid.spookystuff.factory.{HtmlUnitDriverFactory, ChromeDriverFactory, FirefoxDriverFactory}
+import org.tribbloid.spookystuff.factory.{NaiveDriverFactory, HtmlUnitDriverFactory, ChromeDriverFactory, FirefoxDriverFactory}
 
 /**
  * Created by peng on 22/06/14.
@@ -16,11 +16,11 @@ trait SpookyTestCore extends FunSuite {
   lazy val appName = this.getClass.getName
   lazy val conf: SparkConf = new SparkConf().setAppName(appName)
     .setMaster("local[1,3]")
-    .setSparkHome(System.getenv("SPARK_HOME"))
+//    .setSparkHome(System.getenv("SPARK_HOME"))
 
   lazy val sc: SparkContext = new SparkContext(conf)
   lazy val sql: SQLContext = new SQLContext(sc)
-  lazy val spooky: SpookyContext = new SpookyContext(sql, ChromeDriverFactory)
+  lazy val spooky: SpookyContext = new SpookyContext(sql, NaiveDriverFactory)
   val result = doMain()
 
   def doMain(): SchemaRDD
