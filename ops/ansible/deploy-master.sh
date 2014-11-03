eb29d5d41c5c5878ea78e558247bb98ea9a01170
Peng Cheng <pc175@uow.edu.au>
2014-11-03 17:06:26 -0500

    remove all ops files, they are moved into an independent project

--- ops/ansible/deploy-master.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#make sure your password-less login works first
-ansible-playbook deploy-master.yml -i ./inventories --private-key=$HOME/.ssh/id_rsa
--- ops/ansible/deploy-worker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#make sure your password-less login works first
-ansible-playbook deploy-worker.yml -i ./inventories --private-key=$HOME/.ssh/id_rsa
