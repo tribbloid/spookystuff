a232351f2b563ce4135b12f91abf96d4a0fba5b7
Peng Cheng <pc175@uow.edu.au>
2014-11-22 19:44:19 -0500

    fix all package names in repl

--- shell/src/main/scala/org/tribbloid/spookystuff/repl/SpookyILoop.scala
+++ shell/src/main/scala/org/tribbloid/spookystuff/repl/SpookyILoop.scala
@@ -1,17 +1,41 @@
 package org.tribbloid.spookystuff.repl
 
 import org.apache.spark.repl.SparkILoop
+import org.apache.spark.sql.SQLContext
+import org.tribbloid.spookystuff.SpookyContext
+
+import scala.collection.immutable
+import scala.tools.nsc.interpreter._
 
 class SpookyILoop extends SparkILoop {
 
   private val postInitScript =
-    "import org.tribbloid.spookystuff.SpookyContext._" :: //TODO: this is obsolete, revise according to ISpark counterpart
-      "import org.tribbloid.spookystuff.entity._" ::
+    "import scala.concurrent.duration._" ::
+      "import org.tribbloid.spookystuff.actions._" ::
+      "import org.tribbloid.spookystuff.factory.driver._" ::
+      "import org.tribbloid.spookystuff.expressions._" ::
+      "import org.tribbloid.spookystuff.SpookyContext" ::
+      "import sql._" ::
+      "import spooky._" ::
       Nil
 
   override protected def postInitialization() {
     super.postInitialization()
     this.beQuietDuring {
+      val sql = new SQLContext(this.sparkContext)
+      val sqlParam = NamedParam[SQLContext]("sql", sql)
+      intp.bind(sqlParam.name, sqlParam.tpe, sqlParam.value, immutable.List("@transient")) match {
+        case IR.Success =>
+        case _ => throw new RuntimeException("SQL failed to initialize")
+      }
+
+      val spooky = new SpookyContext(sql)
+      val spookyParam = NamedParam[SpookyContext]("spooky", spooky)
+      intp.bind(spookyParam.name, spookyParam.tpe, spookyParam.value, immutable.List("@transient")) match {
+        case IR.Success =>
+        case _ => throw new RuntimeException("SpookyContext failed to initialize")
+      }
+
       postInitScript.foreach(command)
     }
   }
@@ -24,7 +48,7 @@ class SpookyILoop extends SparkILoop {
         |      ____                / /
         |     / __/__  ___  ___   / /__ _   _
         |    _\ \/ _ \/ _ \/ _ \ /  '_// |_/ /
-        |   /___/ .__/\___/\___//_/\_\ \_.  /  STUFF  version 0.2.0
+        |   /___/ .__/\___/\___//_/\_\ \_.  /  STUFF  version 0.3.0-SNAPSHOT
         |      / /                      _/ /
         |     / /                      /__/
 
