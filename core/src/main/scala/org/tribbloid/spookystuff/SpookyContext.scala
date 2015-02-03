a26b65ecb64596b410b7a64bea8a1c486dd3f6af
Peng Cheng <pc175@uow.edu.au>
2015-02-02 19:23:03 -0500

    all metrics no longer relies on SparkContext

--- core/src/main/scala/org/tribbloid/spookystuff/SpookyContext.scala
+++ core/src/main/scala/org/tribbloid/spookystuff/SpookyContext.scala
@@ -65,10 +65,10 @@ class SpookyContext (
 
   import views._
 
-  var metrics = new Metrics(this.sqlContext.sparkContext)
+  var metrics = new Metrics()
 
   def cleanMetrics(): Unit = {
-    metrics = new Metrics(this.sqlContext.sparkContext)
+    metrics = new Metrics()
   }
 
   val hConfWrapper =  if (sqlContext!=null) new SerializableWritable(this.sqlContext.sparkContext.hadoopConfiguration)
@@ -174,28 +174,28 @@ case class DirConf(
 }
 
 //TODO: metrics should not be broadcasted like rest of SpookyContext!
-class Metrics(sc: SparkContext) extends Serializable {
+class Metrics() extends Serializable {
 
   //works but temporarily disabled as not part of 'official' API
-  //  private def accumulator[T](initialValue: T, name: String)(implicit param: AccumulatorParam[T]) = {
-  //    new Accumulator(initialValue, param, Some(name))
-  //  }
+    private def accumulator[T](initialValue: T, name: String)(implicit param: AccumulatorParam[T]) = {
+      new Accumulator(initialValue, param, Some(name))
+    }
 
   import org.apache.spark.SparkContext._
 
-  val driverInitialized: Accumulator[Int] = sc.accumulator(0, "driverInitialized")
-  val driverReclaimed: Accumulator[Int] = sc.accumulator(0, "driverReclaimed")
+  val driverInitialized: Accumulator[Int] = accumulator(0, "driverInitialized")
+  val driverReclaimed: Accumulator[Int] = accumulator(0, "driverReclaimed")
 
-  val sessionInitialized: Accumulator[Int] = sc.accumulator(0, "sessionInitialized")
-  val sessionReclaimed: Accumulator[Int] = sc.accumulator(0, "sessionReclaimed")
+  val sessionInitialized: Accumulator[Int] = accumulator(0, "sessionInitialized")
+  val sessionReclaimed: Accumulator[Int] = accumulator(0, "sessionReclaimed")
 
-  val DFSReadSuccess: Accumulator[Int] = sc.accumulator(0, "DFSReadSuccess")
-  val DFSReadFail: Accumulator[Int] = sc.accumulator(0, "DFSReadFail")
+  val DFSReadSuccess: Accumulator[Int] = accumulator(0, "DFSReadSuccess")
+  val DFSReadFail: Accumulator[Int] = accumulator(0, "DFSReadFail")
 
-  val DFSWriteSuccess: Accumulator[Int] = sc.accumulator(0, "DFSWriteSuccess")
-  val DFSWriteFail: Accumulator[Int] = sc.accumulator(0, "DFSWriteFail")
+  val DFSWriteSuccess: Accumulator[Int] = accumulator(0, "DFSWriteSuccess")
+  val DFSWriteFail: Accumulator[Int] = accumulator(0, "DFSWriteFail")
 
-  val pagesFetched: Accumulator[Int] = sc.accumulator(0, "pagesFetched")
-  val pagesFetchedFromWeb: Accumulator[Int] = sc.accumulator(0, "pagesFetchedFromWeb")
-  val pagesFetchedFromCache: Accumulator[Int] = sc.accumulator(0, "pagesFetchedFromCache")
+  val pagesFetched: Accumulator[Int] = accumulator(0, "pagesFetched")
+  val pagesFetchedFromWeb: Accumulator[Int] = accumulator(0, "pagesFetchedFromWeb")
+  val pagesFetchedFromCache: Accumulator[Int] = accumulator(0, "pagesFetchedFromCache")
 }
