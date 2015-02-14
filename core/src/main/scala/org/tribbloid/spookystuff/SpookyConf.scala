7854681729ffeea11ac4f25a5417822c8dfcf83b
Peng Cheng <pc175@uow.edu.au>
2015-02-14 16:55:58 -0500

    profiling: changed to MEMROY&DISK_SER

--- core/src/main/scala/org/tribbloid/spookystuff/SpookyConf.scala
+++ core/src/main/scala/org/tribbloid/spookystuff/SpookyConf.scala
@@ -99,7 +99,7 @@ class SpookyConf (
                    var checkpointInterval: Int = 50,
                    var batchSize: Int = 500,
 
-                   var defaultStorageLevel: StorageLevel = StorageLevel.MEMORY_ONLY
+                   var defaultStorageLevel: StorageLevel = StorageLevel.MEMORY_AND_DISK_SER
                    ) extends Serializable {
 
   //  def toJSON: String = { //useless for non-case class
