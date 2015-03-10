618e506b2bae3ca14489691e86318e9db851f0d6
Peng Cheng <pc175@uow.edu.au>
2015-03-09 23:00:26 -0400

    add the 'double click' failed test case

--- core/src/test/scala/org/tribbloid/spookystuff/actions/TestInteraction.scala
+++ core/src/test/scala/org/tribbloid/spookystuff/actions/TestInteraction.scala
@@ -3,6 +3,8 @@ package org.tribbloid.spookystuff.actions
 import org.tribbloid.spookystuff.SpookyEnvSuite
 import org.tribbloid.spookystuff.pages.Page
 
+import scala.concurrent.duration
+import scala.concurrent.duration._
 /**
  * Created by peng on 2/19/15.
  */
@@ -20,5 +22,14 @@ class TestInteraction extends SpookyEnvSuite {
 
     val markup = results(0).asInstanceOf[Page].code.get
     assert(markup.contains("<title>Wikipedia</title>"))
+  }
+
+  test("click should not double click") {
+    val results = Trace(
+      (Visit("https://ca.vwr.com/store/search?&pimId=582903")
+        +> Paginate("a[title=Next]", delay = 0.second)).head.self
+    ).resolve(spooky)
+
+    assert(results.size == 5)
   }
 }
