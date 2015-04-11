a1af7b1ba16b5d25154e3bfd3cb4932300eb2dba
Peng Cheng <pc175@uow.edu.au>
2015-04-10 20:03:02 -0400

    add shell component back

---
+++ shell/src/main/scala/org/tribbloid/spookystuff/repl/SpookyILoop.scala
@@ -0,0 +1,8 @@
+package org.tribbloid.spookystuff.repl
+
+import org.apache.spark.repl.SparkILoop
+
+/**
+ * Created by peng on 4/10/15.
+ */
+class SpookyILoop extends SpookyILoopHelper
---
+++ shell/src/main/scala/org/tribbloid/spookystuff/repl/SpookyILoopHelper.scala
@@ -0,0 +1,40 @@
+package org.tribbloid.spookystuff.repl
+
+import org.apache.spark.repl.SparkILoop
+import org.apache.spark.sql.SQLContext
+
+import scala.collection.immutable
+import scala.tools.nsc.interpreter.{Results, NamedParam}
+
+trait SpookyILoopHelper extends SparkILoop { //TODO: merge ispooky initialization into this to avoid recompling
+
+  private val spookyScript =
+    "import scala.concurrent.duration._" ::
+      "import org.tribbloid.spookystuff.actions._" ::
+      "import org.tribbloid.spookystuff.dsl._" ::
+      "import org.tribbloid.spookystuff.SpookyContext" ::
+      "val spooky = new SpookyContext(sql)" ::
+      "import spooky._" ::
+      Nil
+
+  def loadSpooky(): Unit = {
+    this.beQuietDuring {
+      val sql = new SQLContext(this.sparkContext)
+      val sqlParam = NamedParam[SQLContext]("sql", sql)
+      this.bind(sqlParam.name, sqlParam.tpe, sqlParam.value, immutable.List("@transient")) match {
+        case Results.Success =>
+        case _ => throw new RuntimeException("SQL failed to initialize")
+      }
+
+      spookyScript.foreach(s => assert(this.interpret(s) == Results.Success))
+    }
+  }
+
+  override protected def postInitialization() = {
+
+    super.postInitialization()
+    loadSpooky()
+  }
+
+  override def prompt: String = "spooky> "
+}
