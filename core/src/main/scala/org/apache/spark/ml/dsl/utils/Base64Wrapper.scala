a68d2e3b43a163f34b664538d2cbcdd791c59bf1
peng <pc175@uow.edu.au>
2016-08-03 21:15:57 -0400

    update travis

--- core/src/main/scala/org/apache/spark/ml/dsl/utils/Base64Wrapper.scala
+++ core/src/main/scala/org/apache/spark/ml/dsl/utils/Base64Wrapper.scala
@@ -1,7 +1,5 @@
 package org.apache.spark.ml.dsl.utils
 
-import java.util.Base64
-
 /**
  * Created by peng on 4/5/15.
  */
@@ -10,7 +8,7 @@ case class Base64Wrapper(str: String) {
 
   def this(blob: Array[Byte]) {
 
-    this(Base64.getEncoder.encodeToString(blob))
+    this(java.util.Base64.getEncoder.encodeToString(blob))
   }
 
   override def toString = str
@@ -26,5 +24,5 @@ case class Base64Wrapper(str: String) {
 
   def asBase64Str = str
 
-  def asBytes = Base64.getDecoder.decode(str)
+  def asBytes = java.util.Base64.getDecoder.decode(str)
 }
