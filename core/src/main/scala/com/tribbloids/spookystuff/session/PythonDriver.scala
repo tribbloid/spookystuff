2d87a46ca56132a16a1ff9c559558aea02dd61ae
peng <pc175@uow.edu.au>
2016-08-13 16:12:05 -0400

    remove a useless import

--- core/src/main/scala/com/tribbloids/spookystuff/session/PythonDriver.scala
+++ core/src/main/scala/com/tribbloids/spookystuff/session/PythonDriver.scala
@@ -3,6 +3,12 @@ package com.tribbloids.spookystuff.session
 /**
   * Created by peng on 01/08/16.
   */
-class PythonDriver {
+class PythonDriver(
+                    binPath: String
+                  ) extends PythonProcess(binPath) with Clean {
 
+  //  def
+  override def clean(): Unit = {
+    this.close()
+  }
 }
--- core/src/test/scala/com/tribbloids/spookystuff/session/PythonProcessSuite.scala
+++ core/src/test/scala/com/tribbloids/spookystuff/session/PythonProcessSuite.scala
@@ -1,7 +1,6 @@
 package com.tribbloids.spookystuff.session
 
 import com.tribbloids.spookystuff.SpookyEnvSuite
-import com.tribbloids.spookystuff.utils.PythonProcess
 
 /**
   * Created by peng on 01/08/16.
@@ -31,7 +30,8 @@ class PythonProcessSuite extends SpookyEnvSuite {
   }
 
   test("should work in multiple threads") {
-    val rdd = sc.parallelize(1 to 1000)
+    val rdd = sc.parallelize(1 to 100)
+    assert(rdd.getNumPartitions > 1)
     rdd.foreachPartition{
       it =>
         val seq = it.toSeq
