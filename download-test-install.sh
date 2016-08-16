76a814c23f54dbf9069455f62e97b704355ed342
peng <pc175@uow.edu.au>
2016-08-16 18:21:33 -0400

    add download-test-install.sh that setup spark environment

---
+++ download-test-install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
+SPARK_NAME="${SPARK_NAME:?spark-1.6.2}"
+
+SPARK_DIR_NAME="$SPARK_NAME"-bin-hadoop2.4
+
+# Download Spark
+wget -nc http://mirror.csclub.uwaterloo.ca/apache/spark/"$SPARK_NAME"/"$SPARK_DIR_NAME".tgz
+tar -xvzf "$SPARK_DIR_NAME".tgz
+
+SPARK_HOME="$FWDIR"/"$SPARK_DIR_NAME"
+
+./test-install.sh
