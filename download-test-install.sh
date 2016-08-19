ef510db1b6456deac94fb3f8f092539ef5828149
peng <pc175@uow.edu.au>
2016-08-19 04:23:43 -0400

    fix a current dir error

--- download-test-install.sh
+++ download-test-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
+CRDIR="$(cd "`dirname "$0"`"; pwd)"
 SPARK_NAME="${SPARK:-spark-1.6.2}"
 
 SPARK_DIR_NAME="$SPARK_NAME"-bin-hadoop2.4
@@ -9,6 +9,7 @@ SPARK_DIR_NAME="$SPARK_NAME"-bin-hadoop2.4
 wget -nc http://mirror.csclub.uwaterloo.ca/apache/spark/"$SPARK_NAME"/"$SPARK_DIR_NAME".tgz
 tar -xzf "$SPARK_DIR_NAME".tgz
 
-export SPARK_HOME="$FWDIR"/"$SPARK_DIR_NAME"
+export SPARK_HOME="$CRDIR"/"$SPARK_DIR_NAME"
+echo $SPARK_HOME
 
 ./test-install.sh
