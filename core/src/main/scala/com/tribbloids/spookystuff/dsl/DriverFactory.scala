f56053f3a7155549443fcf964f9701170d7bda13
peng <pc175@uow.edu.au>
2016-09-04 02:44:51 -0400

    refix the maven test exclusion that was destroyed by refactoring test resources are now only extracted from jar once

--- core/src/main/scala/com/tribbloids/spookystuff/dsl/DriverFactory.scala
+++ core/src/main/scala/com/tribbloids/spookystuff/dsl/DriverFactory.scala
@@ -41,7 +41,7 @@ import scala.collection.mutable
 // if from the same session release the existing one immediately.
 // if from a different session but same taskAttempt wait for the old one to be released.
 // in any case it should ensure 1 taskAttempt only has 1 active driver
-//TODO: delay Future-based waiting control until asynchonous Action exe is implemented. Right now it works just fine
+//TODO: delay Future-based waiting control until asynchronous Action exe is implemented. Right now it works just fine
 abstract class DriverFactory[+T] extends Serializable {
 
   // If get is called again before the previous driver is released, the old driver is destroyed to create a new one.
--- core/src/test/scala/com/tribbloids/spookystuff/testutils/LocalPathDocsFixture.scala
+++ core/src/test/scala/com/tribbloids/spookystuff/testutils/LocalPathDocsFixture.scala
@@ -2,13 +2,14 @@ package com.tribbloids.spookystuff.testutils
 
 import com.tribbloids.spookystuff.utils.SpookyUtils
 
-trait LocalPathDocsFixture extends RemoteDocsFixture {
+object LocalPathDocsFixture {
 
   //TODO: this should be within TEMP_PATH, however current temp directory cleanup is broken and may results in resources extracted in new suite being deleted by previous suite
   final val TESTUTILS_TEMP_PATH = TestHelper.TARGET_PATH + "resources/testutils"
   final val RESOURCE_NAME = "testutils"
 
-  {
+  // run once per test TODO: or clean up at shutdown hook
+  lazy val testResource = {
     val resourceOpt = SpookyUtils.getCPResource(RESOURCE_NAME)
     resourceOpt.foreach {
       resource =>
@@ -17,18 +18,23 @@ trait LocalPathDocsFixture extends RemoteDocsFixture {
         )
     }
   }
+}
 
-  def unpack(resource: String): String = {
-    TESTUTILS_TEMP_PATH + resource.stripPrefix(RESOURCE_NAME)
+trait LocalPathDocsFixture extends RemoteDocsFixture {
+
+  LocalPathDocsFixture.testResource
+
+  def unpacked(resource: String): String = {
+    LocalPathDocsFixture.TESTUTILS_TEMP_PATH + resource.stripPrefix(LocalPathDocsFixture.RESOURCE_NAME)
   }
 
-  val _HTML_URL = unpack("testutils/site/Wikipedia.html")
-  val _JSON_URL = unpack("testutils/site/tribbloid.json")
-  val _PNG_URL =  unpack("testutils/site/logo11w.png")
-  val _PDF_URL = unpack("testutils/site/Test.pdf")
-  val _XML_URL = unpack("testutils/site/pom.xml")
-  val _CSV_URL = unpack("testutils/site/table.csv")
-  val _DIR_URL = unpack("testutils/site")
+  val _HTML_URL = unpacked("testutils/site/Wikipedia.html")
+  val _JSON_URL = unpacked("testutils/site/tribbloid.json")
+  val _PNG_URL =  unpacked("testutils/site/logo11w.png")
+  val _PDF_URL = unpacked("testutils/site/Test.pdf")
+  val _XML_URL = unpacked("testutils/site/pom.xml")
+  val _CSV_URL = unpacked("testutils/site/table.csv")
+  val _DIR_URL = unpacked("testutils/site")
 
   override def HTML_URL = _HTML_URL
   override def JSON_URL = _JSON_URL
