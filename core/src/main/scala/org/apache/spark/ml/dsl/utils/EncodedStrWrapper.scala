f7b25e7f9f74182991ee9d3689f8c8cb4f2b980c
peng <pc175@uow.edu.au>
2016-11-08 21:55:26 -0500

    all EncodedStrWrappers are no longer case classes but inherit from IDMixin fix a typo in Base16Wrapper

--- core/src/main/scala/org/apache/spark/ml/dsl/utils/EncodedStrWrapper.scala
+++ core/src/main/scala/org/apache/spark/ml/dsl/utils/EncodedStrWrapper.scala
@@ -2,10 +2,12 @@ package org.apache.spark.ml.dsl.utils
 
 import javax.xml.bind.DatatypeConverter
 
+import com.tribbloids.spookystuff.utils.IDMixin
+
 /**
   * Created by peng on 31/10/16.
   */
-abstract class EncodedStrWrapper {
+abstract class EncodedStrWrapper extends IDMixin {
 
   def blob: Array[Byte]
   def asBytes = blob
@@ -15,24 +17,25 @@ abstract class EncodedStrWrapper {
   lazy val toUTF8Str = new String(blob, "UTF-8")
 
   def str: String
+  def _id = str
   override def toString = str
 
   def map(f: String => String): EncodedStrWrapper
 }
 
-case class Base64Wrapper(blob: Array[Byte]) extends EncodedStrWrapper {
+class Base64Wrapper(val blob: Array[Byte]) extends EncodedStrWrapper {
 
   def this(str: String) {
 
     this(DatatypeConverter.parseBase64Binary(str))
   }
 
-  def asBase64Str = toBase16Str
+  def asBase64Str = toBase64Str
   override def str: String = asBase64Str
   def map(f: String => String) = new Base64Wrapper(f(this.str))
 }
 
-case class Base16Wrapper(blob: Array[Byte]) extends EncodedStrWrapper {
+class Base16Wrapper(val blob: Array[Byte]) extends EncodedStrWrapper {
 
   def this(str: String) {
 
@@ -44,7 +47,7 @@ case class Base16Wrapper(blob: Array[Byte]) extends EncodedStrWrapper {
   def map(f: String => String) = new Base16Wrapper(f(this.str))
 }
 
-case class UTF8Wrapper(blob: Array[Byte]) extends EncodedStrWrapper {
+class UTF8Wrapper(val blob: Array[Byte]) extends EncodedStrWrapper {
 
   def this(str: String) {
 
