70b76a23d222d589387ca0f0f80da77fa7dc2dfa
peng <pc175@uow.edu.au>
2017-02-02 10:44:12 -0500

    fix a test memory overhead error

--- core/src/test/scala/com/tribbloids/spookystuff/testutils/TestHelper.scala
+++ core/src/test/scala/com/tribbloids/spookystuff/testutils/TestHelper.scala
@@ -105,7 +105,7 @@ class TestHelper() {
   }
 
   final val TOTAL_MEMORY_CAP = 32768
-  final def executorMemoryCapOpt = clusterSizeOpt.map(TOTAL_MEMORY_CAP / _)
+  final private def executorMemoryCapOpt = clusterSizeOpt.map(v => TOTAL_MEMORY_CAP / v - 256)
   def executorMemoryOpt: Option[Int] = for (n <- numCoresPerWorkerOpt; c <- executorMemoryCapOpt) yield {
     Math.min(n*1024, c)
   }
