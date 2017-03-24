1b0d609f698abe10508be0abbba580a5e5f7380c
peng <pc175@uow.edu.au>
2017-03-24 10:44:04 -0400

    fix a test exclusively for 1.6

--- core/src/test/scala/com/tribbloids/spookystuff/utils/SpookyViewsSuite.scala
+++ core/src/test/scala/com/tribbloids/spookystuff/utils/SpookyViewsSuite.scala
@@ -126,7 +126,7 @@ class SpookyViewsSuite extends SpookyEnvFixture {
 
   test("result of allTaskLocationStrs can be used as partition's preferred location") {
     //TODO: change to more succinct ignore
-    if (org.apache.spark.SPARK_VERSION.replaceAllLiterally(".","").toInt >= 16) {
+    if (org.apache.spark.SPARK_VERSION.replaceAllLiterally(".","").toInt >= 160) {
       val tlStrs = sc.allTaskLocationStrs
       tlStrs.foreach(println)
       val length = tlStrs.size
