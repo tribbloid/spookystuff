8dc5655d39d62d33664191edcb0fb2356fad5da1
peng <pc175@uow.edu.au>
2017-03-25 18:06:19 -0400

    minor fix to build script

--- build-tools/test-install.sh
+++ build-tools/test-install.sh
@@ -2,7 +2,7 @@
 
 FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
 
-mvn dependency:tree -f "$FWDIR"  -Pacceptance -Pdist "$@" > mvnTree.log
+mvn dependency:tree -f "$FWDIR" -Puav -Pdist "$@" > mvnTree.log
 
 MAVEN_OPTS="-Xmx4g -XX:MaxPermSize=4g -XX:ReservedCodeCacheSize=512m" \
 mvn clean install -f "$FWDIR" -Pdist "$@"
