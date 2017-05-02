f66ca06b4228a23ead9750d0b13560e7bd80c961
peng <pc175@uow.edu.au>
2017-05-02 16:59:54 -0400

    add indentation capability to PrettyProduct

--- core/src/main/scala/com/tribbloids/spookystuff/utils/PrettyProduct.scala
+++ core/src/main/scala/com/tribbloids/spookystuff/utils/PrettyProduct.scala
@@ -5,29 +5,35 @@ import java.io.File
 import com.tribbloids.spookystuff.extractors.Literal
 import org.apache.spark.ml.dsl.utils.Verbose
 
-import scala.runtime.ScalaRunTime
-
 /**
   * Created by peng on 01/04/16.
   */
-
+//TODO: merge into MessageAPI
 object PrettyProduct {
 
   def product2String(
-                     x: Product,
-                     start: String = "(",
-                     sep: String = ",",
-                     end: String = ")"
-                   ): String = {
+                      x: Product,
+                      start: String = "(",
+                      sep: String = ",",
+                      end: String = ")",
+                      indentFn: Int => String = _ => "",
+                      recursion: Int = 0
+                    ): String = {
     x match {
       case v: Literal[_, _] => v.toString
       case _ =>
-        val nonVerbose = x.productIterator
+        val strs = x.productIterator
           .map {
             case vv: PrettyProduct => vv.product2String(start, sep, end) //has verbose over
-            case vv: Product => product2String(vv, start, sep, end)
+            case vv: Product => product2String(vv, start, sep, end, indentFn, recursion + 1)
             case vv@ _ => "" + vv
           }
+          .map {
+            str =>
+              val indent = indentFn(recursion)
+              indent + str
+          }
+        val nonVerbose = strs
           .mkString(x.productPrefix + start, sep, end)
         nonVerbose
     }
