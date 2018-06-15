980c5222975c08ac070419e2323fe94eb750f29a
peng <pc175@uow.edy.au>
2018-06-15 12:41:35 -0400

    change timestamp format

--- build-tools/test-install.sh
+++ build-tools/test-install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
+DATE=$(date --iso-8601=second)
 
-mvn dependency:tree -f "$FWDIR"/pom.xml -Puav -Pdist "$@" > "$FWDIR"/mvnTree_"$(date +"%s")".log
+mvn dependency:tree -f "$FWDIR"/pom.xml -Puav -Pdist "$@" > "$FWDIR"/mvnTree_"$DATE".log
 
 MAVEN_OPTS="-Xmx4g -XX:MaxPermSize=4g -XX:ReservedCodeCacheSize=512m" \
 mvn clean install -f "$FWDIR"/pom.xml -Pdist "$@"
--- build-tools/test.sh
+++ build-tools/test.sh
@@ -2,7 +2,5 @@
 
 FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
 
-mvn dependency:tree -f "$FWDIR"/pom.xml -Puav -Pdist "$@" > "$FWDIR"/mvnTree_"$(date +"%s")".log
-
 MAVEN_OPTS="-Xmx4g -XX:MaxPermSize=4g -XX:ReservedCodeCacheSize=512m" \
 mvn test -f "$FWDIR"/pom.xml -Pdist "$@"
