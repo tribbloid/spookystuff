a80961b50270522752cef5f066d2ac50340249d2
Peng Cheng <pc175@uowmail.edu.au>
2019-08-16 11:20:45 -0400

    set SPARK_LOCAL_HOSTNAME # https://stackoverflow.com/questions/49143271/invalid-spark-url-in-local-spark-session

--- build-tools/test-install.sh
+++ build-tools/test-install.sh
@@ -5,5 +5,8 @@ DATE=$(date --iso-8601=second)
 
 mvn dependency:tree -f "$FWDIR"/pom.xml -Puav -Pdist "$@" > "$FWDIR"/mvnTree_"$DATE".log
 
+# mandatory after Spark 2.3
+# https://stackoverflow.com/questions/49143271/invalid-spark-url-in-local-spark-session
 MAVEN_OPTS="-Xmx4g -XX:MaxPermSize=4g -XX:ReservedCodeCacheSize=512m" \
+SPARK_LOCAL_HOSTNAME=localhost \
 mvn clean install -f "$FWDIR"/pom.xml -Pdist "$@"
--- build-tools/test.sh
+++ build-tools/test.sh
@@ -2,5 +2,8 @@
 
 FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
 
+# mandatory after Spark 2.3
+# https://stackoverflow.com/questions/49143271/invalid-spark-url-in-local-spark-session
 MAVEN_OPTS="-Xmx4g -XX:MaxPermSize=4g -XX:ReservedCodeCacheSize=512m" \
+SPARK_LOCAL_HOSTNAME=localhost \
 mvn test -f "$FWDIR"/pom.xml -Pdist "$@"
