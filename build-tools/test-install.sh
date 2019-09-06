7e250a5c159ce13c31f805fae228db9dfd50920d
peng <pc175@uow.edu.au>
2019-09-05 22:35:45 -0400

    mvnTree logs moved into a (preferrably excluded) dir

--- build-tools/test-install.sh
+++ build-tools/test-install.sh
@@ -3,7 +3,7 @@
 FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
 DATE=$(date --iso-8601=second)
 
-mvn dependency:tree -f "$FWDIR"/pom.xml -Puav -Pdist "$@" > "$FWDIR"/mvnTree_"$DATE".log
+mvn dependency:tree -f "$FWDIR"/pom.xml -Puav -Pdist "$@" > "$FWDIR"/logs/mvnTree_"$DATE".log
 
 # mandatory after Spark 2.3
 # https://stackoverflow.com/questions/49143271/invalid-spark-url-in-local-spark-session
