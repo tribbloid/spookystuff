9d9b09d23b0fac7fc7d0f9d5a4faec2a9f62a8ba
peng <pc175@uow.edu.au>
2020-03-09 15:35:53 -0400

    main version bumped to 0.6.1-SNAPSHOT

--- build-tools/mvn-version.sh
+++ build-tools/mvn-version.sh
@@ -5,4 +5,4 @@ FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
 #mvn versions:use-latest-releases
 
 # TODO: this is rendered obsolete by: https://maven.apache.org/maven-ci-friendly.html
-mvn versions:set -DnewVersion=0.6.0-SNAPSHOT -Puav -Pbenchmark -f "$FWDIR"/pom.xml "$@"
+mvn versions:set -DnewVersion=0.6.1-SNAPSHOT -Puav -Pbenchmark -f "$FWDIR"/pom.xml "$@"
