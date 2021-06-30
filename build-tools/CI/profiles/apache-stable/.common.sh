720ade02fb5f03dbb46d2d06e8bedabd3b12deff
peng <pc175@uow.edu.au>
2021-06-30 19:43:42 -0400

    wget now uses quiet mode

--- build-tools/CI/profiles/apache-stable/.common.sh
+++ build-tools/CI/profiles/apache-stable/.common.sh
@@ -6,7 +6,7 @@ SPARK_DIR_ROOT="$HOME/.ci/spark-dist"
 SPARK_DIR_NAME="$SPARK_NAME"-bin-hadoop2.7
 
 # Download Spark
-wget -N http://archive.apache.org/dist/spark/"$SPARK_NAME"/"$SPARK_DIR_NAME".tgz -P "$SPARK_DIR_ROOT"
+wget -N -q http://archive.apache.org/dist/spark/"$SPARK_NAME"/"$SPARK_DIR_NAME".tgz -P "$SPARK_DIR_ROOT"
 tar -xzf "$SPARK_DIR_ROOT/$SPARK_DIR_NAME".tgz -C "$SPARK_DIR_ROOT"
 
 export SPARK_HOME="$SPARK_DIR_ROOT/$SPARK_DIR_NAME"
