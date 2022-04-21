0b097b9f824691277ee2b79f523d3eae160010b7
Peng Cheng <pc175@uow.edu.au>
2022-04-21 19:22:20 -0400

    Broadcasted no longer requires ClassTag given, getClass is used instead

--- core/src/main/scala/com/tribbloids/spookystuff/utils/Broadcasted.scala
+++ core/src/main/scala/com/tribbloids/spookystuff/utils/Broadcasted.scala
@@ -5,30 +5,38 @@ import org.apache.spark.broadcast.Broadcast
 import org.slf4j.LoggerFactory
 
 import scala.reflect.ClassTag
+import scala.language.implicitConversions
 
 /**
   * Created by peng on 27/01/17.
   */
-case class Broadcasted[T: ClassTag](
+case class Broadcasted[T](
     @transient v: T
 )(implicit
   @transient val sc: SparkContext = SparkContext.getOrCreate())
     extends ShippingMarks {
 
-  var broadcasted: Broadcast[T] = sc.broadcast(v)
+  @transient protected implicit val vCtg: ClassTag[T] = ClassTag(v.getClass)
+
+  var delegate: Broadcast[T] = sc.broadcast(v)
 
   def rebroadcast(): Unit = {
     requireNotShipped()
     try {
-      broadcasted.destroy()
+      delegate.destroy()
     } catch {
       case e: Exception =>
         LoggerFactory.getLogger(this.getClass).error("broadcast cannot be destroyed", e)
     }
-    broadcasted = sc.broadcast(v)
+    delegate = sc.broadcast(v)
   }
 
   def value: T = Option(v).getOrElse {
-    broadcasted.value
+    delegate.value
   }
+}
+
+object Broadcasted {
+
+  implicit def asValue[T](v: Broadcast[T]): T = v.value
 }
