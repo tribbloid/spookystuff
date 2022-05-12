fb6303b7628e340f8f93b6f4dea9632cb7f6a9b1
Peng Cheng <pc175@uow.edu.au>
2022-05-12 18:04:54 -0400

    improve test report

--- dev/CI/stage1-core.sh
+++ dev/CI/stage1-core.sh
@@ -7,6 +7,6 @@ set -e
 # shellcheck source=profiles/apache-stable/.common.sh
 source "${FWDIR}/profiles/${1}/.common.sh"
 
-"$FWDIR"/mvn-install.sh "${MVN_PROFILES[@]}" -Pbenchmark && \
-"$FWDIR"/test.sh "${MVN_PROFILES[@]}" -Pbenchmark || \
+("$FWDIR"/mvn-install.sh "${MVN_PROFILES[@]}" -Pbenchmark && \
+"$FWDIR"/test.sh "${MVN_PROFILES[@]}" -Pbenchmark) || \
 (dev/test-reports.sh && exit 1)
--- dev/test-reports.sh
+++ dev/test-reports.sh
@@ -2,4 +2,11 @@
 
 FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
 
+
+echo ""
+echo "###############"
+echo "# TEST REPORT #"
+echo "###############"
+echo ""
+
 find "${FWDIR}" -wholename "**/scalatest-report.txt" -exec cat {} \;
