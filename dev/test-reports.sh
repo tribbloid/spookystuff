ad524ddaa0db7db669ade74efd1c1dd01981d529
Peng Cheng <pc175@uow.edu.au>
2022-05-14 21:57:21 -0400

    test-reports now also list failed tests at the end

--- dev/test-reports.sh
+++ dev/test-reports.sh
@@ -2,7 +2,6 @@
 
 FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
 
-
 echo ""
 echo "###############"
 echo "# TEST REPORT #"
@@ -13,4 +12,17 @@ cd "${FWDIR}"
 
 #find cannot use symbolic link, so cd is used
 #see https://unix.stackexchange.com/questions/93857/find-does-not-work-on-symlinked-path
-find . -wholename "./**/scalatest-report.txt" -print -exec cat {} \;
+find . -wholename "**/scalatest/scalatest-report.txt" -print -exec cat {} \;
+
+
+echo ""
+echo "###############"
+echo "#    FAILED   #"
+echo "###############"
+echo ""
+
+cd "${FWDIR}"
+
+#find cannot use symbolic link, so cd is used
+#see https://unix.stackexchange.com/questions/93857/find-does-not-work-on-symlinked-path
+find . -wholename "**/scalatest/scalatest-failed.txt" -print -exec cat {} \;
