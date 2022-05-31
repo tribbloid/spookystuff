ca19dbc96e8b4f35c0cd731602d5ee3c16b824e0
Peng Cheng <pc175@uow.edu.au>
2022-05-31 18:27:58 -0400

    selenium-bundle now relocates the entire netty for causing major conflict with spark-core netty
    all relocations now uses the relocated.spookystuff package prefix
    
    dev/tree.sh now also log dependency tree of selenium-bundle

--- dev/tree.sh
+++ dev/tree.sh
@@ -7,5 +7,12 @@ FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
 
 source "${CRDIR}/.mvn-common.sh"
 
-mkdir -p logs
-mvn dependency:tree -Dverbose --batch-mode --errors -f "$FWDIR"/pom.xml -Puav -Pdist "$@" > "$FWDIR"/logs/mvnTree_"$DATE".log
+mkdir -p "$FWDIR"/repackaged/selenium-bundle/logs
+
+mvn dependency:tree -Dverbose --batch-mode --errors -f "$FWDIR"/repackaged/selenium-bundle/pom.xml -Pdist "$@" \
+> "$FWDIR"/repackaged/selenium-bundle/logs/mvnTree_"$DATE".log
+
+mkdir -p "$FWDIR"/logs
+
+mvn dependency:tree -Dverbose --batch-mode --errors -f "$FWDIR"/pom.xml -Pdist "$@" \
+> "$FWDIR"/logs/mvnTree_"$DATE".log
