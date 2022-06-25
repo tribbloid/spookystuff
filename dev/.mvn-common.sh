cc3cc66657a99cc4295340fc4c5c19be567a1a92
peng <peng.cheng@acyclic.ai>
2022-06-25 16:20:02 -0400

    remove an obsolete MAVEN_OPTS

--- dev/.mvn-common.sh
+++ dev/.mvn-common.sh
@@ -5,6 +5,6 @@
 # https://stackoverflow.com/questions/49143271/invalid-spark-url-in-local-spark-session
 export SPARK_LOCAL_HOSTNAME=localhost
 
-export MAVEN_OPTS="-Xmx4g -XX:MaxPermSize=4g -XX:ReservedCodeCacheSize=512m"
+export MAVEN_OPTS="-Xmx4g -XX:ReservedCodeCacheSize=512m"
 
 export DATE=$(date --iso-8601=second)
