77a0488f601fa4b86f51f55d8195c5fd0cfc93f4
peng <pc175@uow.edu.au>
2022-10-13 21:38:47 -0400

    remove test-reports invocation

--- dev/CI/stage1-core.sh
+++ dev/CI/stage1-core.sh
@@ -5,5 +5,4 @@ FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
 # shellcheck source=profiles/apache-stable/.common.sh
 source "${FWDIR}/profiles/${1}/.common.sh"
 
-"$FWDIR"/CI-pipeline.sh "-PnotLocal" "${BUILD_PROFILES[@]}" && \
-("$FWDIR"/test-reports.sh && exit 1)
+"$FWDIR"/CI-pipeline.sh "-PnotLocal" "${BUILD_PROFILES[@]}"
