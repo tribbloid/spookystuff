c39a988a21bbc08bd2a76c5d84c9a8843ed40340
peng <peng.cheng@acyclic.ai>
2023-10-08 19:35:13 -0400

    upgrade more

--- dev/gradle-versions.sh
+++ dev/gradle-versions.sh
@@ -3,6 +3,6 @@
 CRDIR="$(cd "`dirname "$0"`"; pwd)"
 FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
 
-${FWDIR}/gradlew wrapper --gradle-version=8.3
+${FWDIR}/gradlew wrapper --gradle-version=8.4
 
 ${FWDIR}/gradlew dependencyUpdates "$@"
