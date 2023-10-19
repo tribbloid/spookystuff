13fa31ec806565280932ce3e1f6e4768b065e9ee
peng <peng.cheng@acyclic.ai>
2023-10-19 11:14:22 -0400

    re-enable wget silent mode
    upgrade CI scripts, local-cluster mode is enabled again

--- dev/update-submodules.sh
+++ dev/update-submodules.sh
@@ -3,4 +3,5 @@
 # this is required if any git module has been initialised to another remote repo
 git submodule sync && \
 git submodule foreach git fetch && \
-git submodule update --init --recursive
+git submodule foreach git reset --hard && \
+git submodule update --init --recursive --force
