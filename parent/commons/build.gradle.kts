7f57c3d2c3b04e73e9dfc739b3844592797542e4
peng <peng.cheng@acyclic.ai>
2024-02-23 15:13:11 -0500

    ascii-graphs is gone, redundant

--- parent/commons/build.gradle.kts
+++ parent/commons/build.gradle.kts
@@ -6,7 +6,6 @@ dependencies {
     api(project(":prover-commons:spark")) // TODO: incldue meta?
     testFixturesApi(testFixtures(project(":prover-commons:spark")))
 
-    api("org.scalameta:ascii-graphs_${vs.scala.binaryV}:0.1.2")
     api("io.github.classgraph:classgraph:4.8.165")
 
     api("com.lihaoyi:pprint_${vs.scala.binaryV}:0.8.1")
