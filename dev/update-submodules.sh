91631a2cba0e7278cfdbe32a58ddb74c17ae0c38
Peng Cheng <peng.cheng@acyclic.ai>
2024-11-06 21:55:16 -0500

    unset local fileMode for all submodules

--- dev/update-submodules.sh
+++ dev/update-submodules.sh
@@ -3,5 +3,6 @@
 # this is required if any git module has been initialised to another remote repo
 git submodule sync && \
 git submodule foreach git fetch && \
+git submodule foreach git config --unset core.fileMode && \ # always use global fileMode, required for dual boot Windows/Linux
 git submodule foreach git reset --hard && \
 git submodule update --init --force
