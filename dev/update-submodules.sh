52dbbe5a07d047ff8b0a3b491faea0dde70ba064
tribbloid <peng.cheng@acyclic.ai>
2024-11-11 02:15:00 -0500

    update-submodules.sh now unset core.fileMode

--- dev/update-submodules.sh
+++ dev/update-submodules.sh
@@ -2,7 +2,9 @@
 
 # this is required if any git module has been initialised to another remote repo
 git submodule sync && \
-git submodule foreach git fetch && \
-git submodule foreach git config --unset core.fileMode && \ # always use global fileMode, required for dual boot Windows/Linux
-git submodule foreach git reset --hard && \
+git submodule foreach "git fetch" && \
+git submodule foreach "git reset --hard" && \
+git submodule foreach "git config --unset core.fileMode || :"
+## always use global fileMode, required for dual boot Windows/Linux (as git-bash on windows interpret NTFS permissions differently)
+
 git submodule update --init --force
