4d24c2deb7fd5e9865887cbfd208a8cb86c24b69
Peng Cheng <peng.cheng@acyclic.ai>
2025-09-11 17:27:10 -0400

    --info is always enabled in test

--- dev/test.sh
+++ dev/test.sh
@@ -5,4 +5,12 @@ FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
 
 source "${CRDIR}/.shared.sh"
 
-${FWDIR}/gradlew test --rerun --continue "-PnoUnused" "-Dorg.gradle.parallel=false" "${@}"
+
+# ${FWDIR}/gradlew test                         # runs the test task
+# --info                                        # displays verbose output
+# --rerun-tasks                                # reruns all tasks on every build
+# --continue                                   # continues task execution after failures
+# -PnoUnused                                   # disables checking unused dependencies
+# -Dorg.gradle.parallel=false                  # disables parallel execution of tasks
+# "${@}"                                       # appends any additional arguments passed to the script
+${FWDIR}/gradlew test --info --rerun --continue "-PnoUnused" "-Dorg.gradle.parallel=false" "${@}"
