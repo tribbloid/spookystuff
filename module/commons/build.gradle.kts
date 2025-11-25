2a6b211f3cf1bb340b929af16c74c0369d6af3c7
Peng Cheng <peng.cheng@acyclic.ai>
2025-11-25 18:20:03 -0500

    frameless moved to linq subproj

--- module/commons/build.gradle.kts
+++ module/commons/build.gradle.kts
@@ -9,8 +9,5 @@ dependencies {
 
     api("org.scala-lang.modules:scala-collection-compat_${vs.scala.artifactSuffix}:2.13.0")
 
-// https://mvnrepository.com/artifact/org.typelevel/frameless-dataset
-    api("org.typelevel:frameless-dataset_${vs.scala.binaryV}:0.16.0")
-
     testImplementation(project(":prover-commons:meta2"))
 }
--- module/linq/build.gradle.kts
+++ module/linq/build.gradle.kts
@@ -5,4 +5,6 @@ dependencies {
     api(project(":module:commons"))
     testFixturesApi(testFixtures(project(":module:commons")))
 
+// https://mvnrepository.com/artifact/org.typelevel/frameless-dataset
+    api("org.typelevel:frameless-dataset_${vs.scala.binaryV}:0.16.0")
 }
