c8f1c40a570a6569d4847cac65ca3faa34045ad0
Peng Cheng <peng.cheng@acyclic.ai>
2025-11-27 15:19:42 -0500

    !amend incomplete fix

---
+++ module/commons/src/main/scala/com/tribbloids/spookystuff/io/FilePermissions.scala
@@ -0,0 +1,13 @@
+package com.tribbloids.spookystuff.io
+
+/**
+ * Cross-platform file permission types that can be used across different resolver implementations
+ * (LocalResolver, HDFSResolver, etc.)
+ */
+sealed trait FilePermissionType
+
+object FilePermissionType {
+  case object Executable extends FilePermissionType
+  case object Writable extends FilePermissionType
+  case object Readable extends FilePermissionType
+}
